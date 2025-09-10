5a5c0dd3efbf60de1a1142ff0884a70789be8f25
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-09-09 21:31:23 -0400

    ex1check

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
+        public Program()
+        {
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>()
+            { "GTA.V", "Smash Bros", "007" };
+            
+            videoGames.Add("Minecraft");
+            videoGames.Add("Fortnite");
+
+            var orderedGames = videoGames.OrderBy(name => name.Length);
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
